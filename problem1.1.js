8e6d9f8e6478269b35f659cbccc48e62900cd4c7
Mahmudul Hasan Emon <mhhsan341@gmail.com>
2026-07-09 19:15:23 +0600

    problem1.1 solved

---
+++ problem1.1.js
@@ -0,0 +1,9 @@
+// this is a variable 
+let a =5;
+/* 
+this is full sentence 
+where I write my fav player,team, club name
+*/
+console.log("Leonel Messi")
+console.log("Argentina")
+console.log("FC Barcelona")
