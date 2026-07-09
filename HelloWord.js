985eac48abbfbf719fd76409b0278e763c6d70a7
Mahmudul Hasan Emon <mhhsan341@gmail.com>
2026-07-09 10:48:53 +0600

    Comment added

--- HelloWord.js
+++ HelloWord.js
@@ -1,2 +1,7 @@
 console.log("Hello, World!");
+// Comment
+// output Hello, World! to the console
 console.log("I'm learning JavaScript!");
+// Comment
+// output I'm learning JavaScript! to the console
+
