609dc12ac305a0a0692353fafe61fcd8c0abe88b
Mahmudul Hasan Emon <mhhsan341@gmail.com>
2026-07-10 19:10:34 +0600

    Variables

---
+++ variables.js
@@ -0,0 +1,9 @@
+const name = "Mahmudul Hasan Emon";
+const universityName = "Metropolitan University"; // in const value cann't change
+
+let studentCount = 45; // in let value can be changed
+studentCount = 50; // value change successfully
+
+console.log(name)
+console.log(universityName)
+console.log(studentCount)
