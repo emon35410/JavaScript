fcbc2fc263aaed232c461585f2d1c2e31b3134d1
Mahmudul Hasan Emon <mhhsan341@gmail.com>
2026-07-12 02:43:38 +0600

    Template string

---
+++ TemplateString.js
@@ -0,0 +1,5 @@
+const Name = "Messi";
+const scored = 7;
+
+console.log(`This is ${Name} and he scored ${scored} goals in 2022 FIFA world cup`)
+console.log(`And in 2026 World cup ${Name} already scored ${scored + 1} goals in stil playing in quater finals`)
