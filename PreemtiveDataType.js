0d1910d8b8e36057406a13262b544fa0dff17b0d
Mahmudul Hasan Emon <mhhsan341@gmail.com>
2026-07-12 02:38:18 +0600

    Type of

--- PreemtiveDataType.js
+++ PreemtiveDataType.js
@@ -3,7 +3,7 @@ let number = 88 // Number
 let isStudent = true // Boolean 
 const cupon = null //null
 
-console.log(name)
-console.log(number)
-console.log(isStudent)
-console.log(cupon)
+console.log(name,    typeof name)
+console.log(number, typeof number)
+console.log(isStudent, typeof isStudent)
+console.log(cupon, typeof cupon)
