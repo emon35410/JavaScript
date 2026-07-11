7423c4822bfb27ecc38ad40a3b4f276da8afc7fb
Mahmudul Hasan Emon <mhhsan341@gmail.com>
2026-07-12 02:51:03 +0600

    Type Conversion

---
+++ TypeConversion.js
@@ -0,0 +1,9 @@
+const input = "25";
+const age = Number(input);
+console.log(age + 5 , typeof age)
+
+const taka = 30;
+const label = String(taka);
+console.log(label + "taka", typeof label)
+
+console.log(Number("abcd"), typeof Number("abcd"))
