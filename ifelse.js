f05eee594ceac7304633bded6e9cacc08ccfa065
Mahmudul Hasan Emon <mhhsan341@gmail.com>
2026-07-15 16:15:17 +0600

    If else

---
+++ ifelse.js
@@ -0,0 +1,14 @@
+const marks = 85;
+
+if(marks >=80){
+console.log("A+")
+}
+else if(marks<80 && marks>=70){
+    console.log("A")
+}
+else if(marks<70 && marks>=60){
+    console.log("A-")
+}
+else{
+    console.log("F")
+}
