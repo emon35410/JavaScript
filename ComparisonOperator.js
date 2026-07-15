ff6426642ff16a142c71ea5c9c538b781d9c4e19
Mahmudul Hasan Emon <mhhsan341@gmail.com>
2026-07-15 15:53:11 +0600

    ComparisonOperator.js

---
+++ ComparisonOperator.js
@@ -0,0 +1,6 @@
+console.log(5 == "5"); // check only number not type
+console.log(5 === "5") // check number and type both
+console.log(7 !==5); // !== means not equal
+console.log(10>3) // < geater  > less
+console.log( 10 <= 10) // <= less then or equal
+console.log(10>=10) // >= geater then or equal  
