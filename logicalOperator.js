07169e7531b3b718568b24d844386f0389e41533
Mahmudul Hasan Emon <mhhsan341@gmail.com>
2026-07-15 16:05:29 +0600

    logical operators

--- logicalOperator.js
+++ logicalOperator.js
@@ -1,6 +1,6 @@
 let age = 20;
 const hashID = true;
 
-console.log(age > 18 && hashID);
-console.log(age > 18 || hashID);
-console.log(!hashID);
+console.log(age > 18 && hashID); // AND operation must be both are true
+console.log(age > 18 || hashID); // OR operation at least one should be true
+console.log(!hashID); // flip the value true will be false and false will be true
