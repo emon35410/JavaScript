3c9d00b07d814f71ee11ef9d15b4e712623ae05a
Mahmudul Hasan Emon <mhhsan341@gmail.com>
2026-07-15 14:37:12 +0600

    problem2.2.js done

---
+++ problem2.2.js
@@ -0,0 +1,13 @@
+console.log("5"+3);
+console.log("5"-3);
+
+/* in "5" + 3
++ operation operate two things addition and concatanation
+so in plus operation when the first item is string it add the others item as string 
+for this "5" + 3 = "53"  
+
+in "5" - 3
+- operation only do one things that is minus so in that there is only minus the numbers
+
+
+*/
