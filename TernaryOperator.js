382deb7340301ba059fdf6c0a56fcb9d375dd9e8
Mahmudul Hasan Emon <mhhsan341@gmail.com>
2026-07-15 16:41:10 +0600

    Ternary Operator

---
+++ TernaryOperator.js
@@ -0,0 +1,10 @@
+const balace = 120;
+
+const status = balace >=110 ? "Sufficient" : "Low";
+
+console.log(status);
+
+const marks = 85;
+
+const grade = (marks >= 80) ? "A+" : (marks>=70) ? "A" : "F"; 
+console.log(grade)
