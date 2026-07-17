10baa8171654252a63a96db755943ae5f78591b3
Mahmudul Hasan Emon <mhhsan341@gmail.com>
2026-07-17 21:23:17 +0600

    Looping Over Collection

---
+++ LoopingOverCollection.js
@@ -0,0 +1,5 @@
+const citys =  ["Mecca","Medina","Istanbul","London","Damascuss","Aleppo","Latakia","Ankara","Bagdad"];
+
+for (const city of citys){
+    console.log(`My Dream To Visit ${city}`)
+}
