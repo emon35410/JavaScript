5215429048f8e0ee0acb764e225550def8418dff
Mahmudul Hasan Emon <mhhsan341@gmail.com>
2026-07-22 15:55:23 +0600

    Sum and max min problem solved

--- Array.js
+++ Array.js
@@ -28,4 +28,26 @@ console.log(...nums,10)
 console.log([...nums,...marks])
 
 // The Big Three: map, filter, reduce
+// map
+const num = nums.map(p=>console.log(p))
+const numm = nums.map(p=>p*5);
+console.log(numm);
 
+// sum of array numbers
+const scores = [35,80,55,93,20];
+let sum = 0;
+for(let i =0;i<=scores.length-1;i++){
+    sum += scores[i];
+}
+console.log(sum);
+// find the maximum value of array
+let max =0;
+for(j = 0; j<= scores.length-1;j++){
+    if(max<scores[j])
+    max = scores[j];
+}
+const maximum = Math.max(...scores)
+const minimum = Math.min(...scores)
+console.log(max)
+console.log(maximum)
+console.log(minimum)
